a3a065b311850c2710737d4a4b45198e93b1fff6
cattr1n <130205360+cattr1n@users.noreply.github.com>
2023-06-24 00:59:06 +0500

    update file

---
+++ WindowsActivator3/RestartPrompt.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsActivator3
+{
+    public partial class RestartPrompt : Form
+    {
+        public RestartPrompt()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+        private void Form2_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void siticoneButton2_Click(object sender, EventArgs e)
+        {
+            ActiveForm.Close();
+        }
+
+        private void siticoneButton1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("shutdown.exe", "-r -t 0");
+            ActiveForm.Close();
+        }
+    }
+}
